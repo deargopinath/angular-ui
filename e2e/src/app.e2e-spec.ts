0ad307408c58aab90edb6dd3685bbc42fd92d7d3
Srinivas Gopinath Parimi <deargopinath@gmail.com>
2020-02-26 22:17:03 +0000

    upgraded to angular 9

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular8-demo!');
+    expect(page.getTitleText()).toEqual('angular9-demo app is running!');
   });
 
   afterEach(async () => {
